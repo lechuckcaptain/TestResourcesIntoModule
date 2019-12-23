1188b054022bcc5ee6d35baf462be242f88172a6
Marco Pagliari <lechuckcaptain@gmail.com>
2019-12-23 10:48:12 +0100

    Added test project

---
+++ TestResourcesIntoModule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestResourcesIntoModule
+//
+//  Created by Marco Pagliari on 23/12/2019.
+//  Copyright © 2019 One4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
