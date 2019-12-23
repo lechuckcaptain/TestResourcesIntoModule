97c36b5224336df2633c6763924e367786855cb6
Marco Pagliari <lechuckcaptain@gmail.com>
2019-12-23 11:32:28 +0100

    Added resources into module and reference into main module, with issue on dark mode

---
+++ ResourcesModule/ResourcesModule/ResourcesModule.h
@@ -0,0 +1,19 @@
+//
+//  ResourcesModule.h
+//  ResourcesModule
+//
+//  Created by Marco Pagliari on 23/12/2019.
+//  Copyright © 2019 One4. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ResourcesModule.
+FOUNDATION_EXPORT double ResourcesModuleVersionNumber;
+
+//! Project version string for ResourcesModule.
+FOUNDATION_EXPORT const unsigned char ResourcesModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResourcesModule/PublicHeader.h>
+
+
